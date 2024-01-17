ac8f94ac53ed462eee5c8ac1b1574084815bec9f
Arold Mauger <aroldmauger2022@gmail.com>
2024-01-17 15:03:55 +0100

    Ajout du composant Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./sass/App.scss";
-import Header from './components/Navbar.jsx';
+import Header from './components/Header.jsx';
+import Banner from './components/Banner.jsx';
 
 function App() {
   return (
     <div>
       <Header />
+      <Banner/>
     </div>
   )
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../sass/Banner.scss";
+
+function Banner() {
+  return (
+    <div className="banner">
+        <img className="banner_image" src="picture-main.png" alt="banner du site Kasa montrant un paysage de falaises"/>
+        <h2 className="banner_text">Chez vous, partout et ailleurs</h2>
+    </div>
+  )
+}
+
+export default Banner
