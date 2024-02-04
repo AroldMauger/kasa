1a876d3633260f296f0672a2a179724dd7a40bda
Arold Mauger <aroldmauger2022@gmail.com>
2024-02-04 14:14:26 +0100

    Ouverture et fermeture des collapse au clic sur le chevron

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import "./Collapse.scss";
+import { useState } from 'react';
 
 function Collapse(props) {
+
+  const [isContentVisible, setIsContentVisible] = useState(false)
+  const [isImageOpened, setIsImageOpened] = useState(true);
+
+  const showContent = () => {
+    setIsContentVisible(!isContentVisible);
+    setIsImageOpened(!isImageOpened);
+  }
+
   return (
     <div className="apartment_description">
         <p className="collapse_header">
             <span>{props.title}</span>
-            <img className="drop" src="drop_closed.png"/>
+            <img className="drop" src={isImageOpened ? "drop_closed.png" : "drop_opened.png"} onClick={showContent} alt="bouton en forme de chevron"/>
         </p>
-        <p className="collapse_content">{props.content}</p>
+        {isContentVisible && <p className="collapse_content">{props.content}</p>}
     </div>  
   )
 }
