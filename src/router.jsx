fac1afd59f4ae3a3b4e0999830eab845ad9f3683
Arold Mauger <aroldmauger2022@gmail.com>
2024-04-06 20:22:57 +0200

    Modifications de la route pour HomePage

--- src/router.jsx
+++ src/router.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
     errorElement: <NotFound404/>, 
     children :[
       {
-        path: "/",
+        path: "/kasa",
         element: <HomePage/>
       },
       {
