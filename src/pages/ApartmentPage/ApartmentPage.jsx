4ad1401841418e2890f3a273345258ddf4af9095
Arold Mauger <aroldmauger2022@gmail.com>
2024-04-06 20:54:51 +0200

    Modifications des routes

--- src/pages/ApartmentPage/ApartmentPage.jsx
+++ src/pages/ApartmentPage/ApartmentPage.jsx
@@ -31,7 +31,7 @@ function ApartmentPage() {
 
     {/* Dans fetch on va chercher avec .find l'appartment dont l'id passé dans le location.state correspond à l'id dans le fichier json */}
     function fetchApartmentData () {
-      fetch ("kasa/data.json")
+      fetch ("data.json")
       .then ((response) => response.json())
       .then((apartments) => {
           const apartment = apartments.find((apartment) => apartment.id === location.state.apartmentId);
